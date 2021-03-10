65eecd6eee791f58a060ca40d76ceda5c9f0427a
shlomo glick <shlomog12@gmail.com>
2021-03-10 10:57:53 +0200

    test

---
+++ snowman.cpp
@@ -0,0 +1,21 @@
+#include "snowman.hpp"
+
+#include <iostream>
+#include <stdexcept>
+const int MAX = 99999999; 
+const int MIN = 10000000;
+const int TEN = 10;
+
+const char * ERORR1 = "The number in each index must be between 1 and 4";
+const char * ERORR2 = "The number must be 8 digits long";
+
+
+using namespace std;
+
+
+namespace ariel{
+    string snowman(int n){
+    string ans="Not fulfilled";
+    return ans;
+}
+}
